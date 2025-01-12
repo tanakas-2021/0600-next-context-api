c1d7aab978ba60aac58309bc720dadf91e23fb56
tanakas-2021 <shugotanaka2020@gmail.com>
2025-01-12 20:48:32 +0900

    headerコンポーネント作成

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,36 +1,9 @@
-import styles from "./page.module.scss";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faInfoCircle,
-  faBell,
-  faUserCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { Header } from "@/components/header";
 
 const Page = () => {
   return (
-    <header className={styles.main}>
-      <div className={styles.contents}>
-        <div className={styles.contentsLeft}>Turvo</div>
-        <div className={styles.contentsRight}>
-          <ul className={styles.menu}>
-            <li className={styles.menuItem}>
-              <FontAwesomeIcon icon={faPlus} size="xs" color="white" />
-            </li>
-            <li className={styles.menuItem}>
-              <FontAwesomeIcon icon={faInfoCircle} size="xs" color="white" />
-            </li>
-            <li className={styles.menuItem}>
-              <FontAwesomeIcon icon={faBell} size="xs" color="white" />
-            </li>
-          </ul>
-          <div className={styles.iconUser}>
-            <FontAwesomeIcon icon={faUserCircle} size="xs" color="white" />
-          </div>
-        </div>
-      </div>
-    </header>
+    <Header/>
+    
   );
 };
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,1 +1,34 @@
+import styles from "./index.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faInfoCircle,
+  faBell,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
+export const Header = () => {
+  return (
+    <header className={styles.main}>
+      <div className={styles.contents}>
+        <div className={styles.contentsLeft}>Turvo</div>
+        <div className={styles.contentsRight}>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <FontAwesomeIcon icon={faPlus} size="xs" color="white" />
+            </li>
+            <li className={styles.menuItem}>
+              <FontAwesomeIcon icon={faInfoCircle} size="xs" color="white" />
+            </li>
+            <li className={styles.menuItem}>
+              <FontAwesomeIcon icon={faBell} size="xs" color="white" />
+            </li>
+          </ul>
+          <div className={styles.iconUser}>
+            <FontAwesomeIcon icon={faUserCircle} size="xs" color="white" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
