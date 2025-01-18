2315bec3d55013476f42e072ea6a35a3a0184f28
tanakas-2021 <shugotanaka2020@gmail.com>
2025-01-18 15:03:10 +0900

    ”指摘事項対応_sidebarとheaderは全画面共通のためlayout.tsxに定義

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar";
-import styles from "./page.module.scss";
 import React from "react";
 
 const Page = () => {
-  return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <Sidebar/>
-        <div className={styles.mainContents}></div>
-      </main>
-    </>
-  );
+  return <div>ここがProject Main画面</div>;
 };
 
 export default Page;
