b202e7c19e237afb4464ce920ad1428c00bc5c60
tanakas-2021 <shugotanaka2020@gmail.com>
2025-01-27 19:59:44 +0900

    リファクタリング_axiosインスタンスを使ってbaseURLを共通化

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { projectsResponse } from "../types/types";
 
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api/v1/',
+  timeout: 1000,
+});
+
 export const fetchProjects = async (): Promise<projectsResponse> => {
-  const response = await axios.get<projectsResponse>(
-    "http://localhost:3000/api/v1/users/projects"
+  const response = await instance.get<projectsResponse>(
+    "users/projects"
   );
   return {
     data: response.data.data,
